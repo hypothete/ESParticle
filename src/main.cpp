63bf2d514ac14a0483572e7467778937736bedb4
Duncan Alexander <hypo@hypothete.com>
2020-10-24 14:38:22 -0700

    removed oled library

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include <Adafruit_BMP280.h>
 #include <PMserial.h>
-// #include <U8g2lib.h>
 #include <WebServer.h>
 #include <WiFi.h>
 #include <Wire.h>
@@ -10,16 +9,8 @@
 #define SCL 4
 #define SDA 5
 
-#define I2C_SCL 14
-#define I2C_SDA 12
-
-// U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R0, SCL, SDA, U8X8_PIN_NONE);
-
 SerialPM pms(PMS5003, Serial);
 
-// TwoWire I2CBME = TwoWire(0);
-
-// Adafruit_BMP280 bmp(&I2CBME);
 Adafruit_BMP280 bmp;
 
 WebServer server(80);
@@ -31,8 +22,6 @@ uint16_t ugC = 0;
 float temperature, pressure, altitude;
 
 unsigned long lastTime = 0;
-
-String message = "AAA";
 
 void connectToWifi() {
   WiFi.mode(WIFI_STA);
@@ -82,22 +71,13 @@ void handleNotFound() {
 }
 
 void setupServer() {
-  // start server
   server.on("/", handleGet);
   server.onNotFound(handleNotFound);
   server.begin();
 }
 
-// void drawReadings() {
-//   u8g2.setCursor(0, 16);
-//   u8g2.print(WiFi.localIP());
-//   u8g2.setCursor(0, 26);
-//   u8g2.print(message);
-// }
 
 void setup() {
-  Serial.begin(9600);
-  // I2CBME.begin(I2C_SDA, I2C_SCL, 100000);
   Wire.begin(SDA, SCL, 115200);
   pms.init();
 
@@ -108,13 +88,7 @@ void setup() {
       Adafruit_BMP280::SAMPLING_X16,
       Adafruit_BMP280::FILTER_X16,
       Adafruit_BMP280::STANDBY_MS_500);
-  } else {
-    message = "BMP Not found";
   }
-
-  // u8g2.begin();
-  // u8g2.setFont(u8g2_font_6x10_tr);
-  // u8g2.firstPage();
 
   connectToWifi();
   setupServer();
@@ -129,8 +103,4 @@ void loop() {
   }
 
   server.handleClient();
-  
-  // if (u8g2.nextPage()) {
-  //   drawReadings();
-  // }
 }
